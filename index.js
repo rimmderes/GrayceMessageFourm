69dcbe40b0c45283d7eb8f47d6cb751ff36e16ba
Connel Nwaodor <connel_n@hotmail.com>
2023-04-18 16:31:51 +0100

    json file created

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+// fetch('data.json')
+//     .then((response) => response.json())
+//     .then(data  => { 
+//         console.log(data) 
+//     });
+
+
+    // const forumData = new XMLHttpRequest();
+
+    // forumData.open('GET', 'data.json', true);
+    
+    // forumData.onreadystatechange = function {
+    
+    // if (forumData.readyState === 4 && forumData.status === 400) {
+    
+    //     const datas = JSON.parse(forumData.responseText);
+    
+    //     console.log(datas)      //printing the fetched data
+    
+    //     }
+    
+    // };
+    
+    // datas.send();
+
+
+    // const posts = async () => {
+    //     const response = await fetch("data.json");
+    //     const data = await response.json();
+    
+    //     console.log(data);
+    // }
+
+    // posts();
